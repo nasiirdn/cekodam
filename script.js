f00b0f889ecbac185bcd1b31d54308eca6e93d1e
nasiirdn <108565383+nasiirdn@users.noreply.github.com>
2024-06-29 22:27:57 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+function checkKhodam () {
+    const name = document.getElementById('nameInput').value.trim();
+    const resultDiv = document.getElementById('result');
+
+    if (name === "") {
+        resultDiv.innerHTML = "<p style='color: red;'>Nama tidak boleh kosong</p>";
+        return;
+    }
+
+    const khodamNames = [
+        "Khodam Naga Api",
+        "Khodam Naga Es",
+        "Khodam Naga Air",
+        "Kura-Kura Sad Boy",
+        "Siput Kayang",
+        "Raja Tapir",
+        "Buaya Ngesod"
+    ];
+    
+    const khodamPresent = Math.random() > 0.5;
+
+    if (khodamPresent) {
+        const khodamName = khodamNames[Math.floor(Math.random() * khodamNames.length)];
+        resultDiv.innerHTML = `<p style='color: green'; >${name}, Anda memiliki khodam ${khodamName} yang melindungi anda!<p/>`;
+    } else {
+        resultDiv.innerHTML = `<p style='color: red'; >${name}, maaf anda tidak memiliki khodam. </p>`
+    }
+}
+
+function resetForm() {
+    document.getElementById('nameInput').value = '';
+    document.getElementById('result').innerHTML = '';
+}
